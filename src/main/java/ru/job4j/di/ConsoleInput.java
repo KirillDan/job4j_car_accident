771af8c29df6c063e64d98918f196e87d501f5ff
KirillDan <kirilldan94@mail.ru>
2020-11-14 16:43:48 +0300

    4. Время жизни (Scope) [#364389]

--- src/main/java/ru/job4j/di/ConsoleInput.java
+++ src/main/java/ru/job4j/di/ConsoleInput.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class ConsoleInput {
 	private List<String> inputList = new ArrayList<String>();
 	
--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -4,13 +4,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class SpringDI {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+    	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.scan("ru.job4j.di");
         context.refresh();
-        StartUI ui = context.getBean(StartUI.class);
-        ui.add("Petr Arsentev");
-        ui.add("Ivan ivanov");
-        ui.addFromConsole();
-        ui.print();
+        Store store = context.getBean(Store.class);
+        store.add("Petr Arsentev");
+        Store another = context.getBean(Store.class);
+        another.getAll().forEach(System.out::println);
     }
 }
